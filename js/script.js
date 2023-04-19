5e8dd82ba21b79cec4470902095f7108bd76b2f4
henri-kapidani <h.kapidani@gmail.com>
2023-04-19 15:03:46 +0400

    add thumbs

--- js/script.js
+++ js/script.js
@@ -8,16 +8,22 @@ const arrImages = [
 ];
 
 const containerHighlighted = document.querySelector('.highlighted');
+const containerThumbs = document.querySelector('.thumbs');
 
 for (let i = 0; i < arrImages.length; i++) {
 	containerHighlighted.innerHTML += `<img src="${arrImages[i]}" alt="" class="${i == 0 ? 'active' : ''}">`;
+	containerThumbs.innerHTML += `<img src="${arrImages[i]}" alt="" class="${i == 0 ? 'active' : ''}">`;
 }
 
 
 // selezionimo le immagini nell'html
 const listHighlighted = document.querySelectorAll('.highlighted img');
+// selezioniamo le miniature
+const listThumbs = document.querySelectorAll('.thumbs img');
+// selezioniamo i bottoni
 const btnPrev = document.querySelector('.btn-prev');
 const btnNext = document.querySelector('.btn-next');
+
 
 // definito una variabile che rappresenta lo stato attuale del carosello
 // cioe' l'indice dell'immagine attiva
@@ -27,6 +33,7 @@ btnNext.addEventListener('click',
 	function() {
 		// dall'immagine attiva tolgo la classe active
 		listHighlighted[activeIndex].classList.remove('active');
+		listThumbs[activeIndex].classList.remove('active');
 		// settiamo il nuovo valore di active index
 		activeIndex++;
 		if (activeIndex >= listHighlighted.length) {
@@ -34,6 +41,7 @@ btnNext.addEventListener('click',
 		}
 		// alla nuova immagine attiva aggiungiamo la classe active
 		listHighlighted[activeIndex].classList.add('active');
+		listThumbs[activeIndex].classList.add('active');
 	}
 );
 
@@ -41,6 +49,7 @@ btnPrev.addEventListener('click',
 	function() {
 		// dall'immagine attiva tolgo la classe active
 		listHighlighted[activeIndex].classList.remove('active');
+		listThumbs[activeIndex].classList.remove('active');
 		// settiamo il nuovo valore di active index
 		activeIndex--;
 		if (activeIndex < 0) {
@@ -48,5 +57,31 @@ btnPrev.addEventListener('click',
 		}
 		// alla nuova immagine attiva aggiungiamo la classe active
 		listHighlighted[activeIndex].classList.add('active');
+		listThumbs[activeIndex].classList.add('active');
 	}
 );
+
+// ciclo per aggiungere gli event listeners alle miniature
+for (let i = 0; i < listThumbs.length; i++) {
+	listThumbs[i].addEventListener('click',
+		function() {
+			console.log('cliccata la miniature in posizione ' + i)
+			listHighlighted[activeIndex].classList.remove('active');
+			listThumbs[activeIndex].classList.remove('active');
+			activeIndex = i;
+			listHighlighted[activeIndex].classList.add('active');
+			listThumbs[activeIndex].classList.add('active');
+		}
+	)
+}
+
+
+
+/*
+btnNext ---> al click fai function() {...}
+btnPrev ---> al click fai function() {...}
+thumb0 ----> al click fai function() {...}
+thumb1 ----> al click fai function() {...}
+thumb3 ----> al click fai function() {...}
+thumb4 ----> al click fai function() {...}
+*/
